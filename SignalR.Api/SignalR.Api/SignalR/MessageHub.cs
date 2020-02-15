b2cc6972ba8724d2f144b812b9b72da0ae07e00b
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-02-15 15:56:36 -0600

    Add SignalR to the API - started

---
+++ SignalR.Api/SignalR.Api/SignalR/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Api.SignalR
+{
+	public class MessageHub : Hub
+	{
+		public async Task NewMessage(Message msg)
+		{
+			await Clients.All.SendAsync("MessageReceived", msg);
+		}
+	}
+}
