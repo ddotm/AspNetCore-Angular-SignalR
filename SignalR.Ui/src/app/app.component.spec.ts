f965d2d6ea6b77ca1f5a0f2e823205ee99040ffe
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-02-15 15:06:46 -0600

    added bootstrap

--- SignalR.Ui/src/app/app.component.spec.ts
+++ SignalR.Ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SignalR-Ui'`, () => {
+  it(`should have as title 'signalr-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SignalR-Ui');
+    expect(app.title).toEqual('signalr-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SignalR-Ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('signalr-ui app is running!');
   });
 });
